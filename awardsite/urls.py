5ee343bd740e9b85b5ea2e4103349ccb5133e914
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-25 23:12:39 +0300

    updated structure

--- awardsite/urls.py
+++ awardsite/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from django.conf.urls import url
+from django.contrib.auth import views
+# from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('my_awards.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(), {"next_page": '/'}),
+    url(r'^tinymce/', include('tinymce.urls')),
+    # url(r'^api-token-auth/', obtain_auth_token),
 ]
