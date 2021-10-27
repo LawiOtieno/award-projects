0365aeaf2d9435ae8b5ba876e9fd497c4c24a0a9
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-27 07:05:44 +0300

    secret key

--- awardsite/settings.py
+++ awardsite/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!  django-insecure-4b$f^+xnrin(_xxcq81(ev=*p=mz%+bm_fd2!93g6ygpo=zgjd
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 
